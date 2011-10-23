c465472a310df5d3236248739913a9d2e74cd096
Federico Bond <federicobond@gmail.com>
2011-10-23 11:32:07 -0300

    Añadidos tests para Board

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -3,12 +3,12 @@ package tests;
 import game.Board;
 import game.InvalidBoardSizeException;
 import junit.framework.TestCase;
-
 import misc.Position;
 
 import org.junit.Test;
 
 import tiles.EmptyTile;
+import tiles.Wall;
 
 public class BoardTest extends TestCase {
 
@@ -20,7 +20,7 @@ public class BoardTest extends TestCase {
 
 	@Test
 	public void testBlankBoard() {
-		assertTrue(board.getTile(new Position(0, 0)).getClass() == EmptyTile.class);
+		assertEquals(EmptyTile.class, board.getTile(new Position(0, 0)).getClass());
 	}
 
 	@Test
@@ -35,6 +35,24 @@ public class BoardTest extends TestCase {
 			new Board(26, 26);
 			fail();
 		} catch (InvalidBoardSizeException e) {
+		}
+	}
+	
+	@Test
+	public void testTileAccessors() {
+		board.setTile(new Position(1, 0), new Wall());
+		assertEquals(Wall.class, board.getTile(new Position(1, 0)).getClass());		
+	}
+	
+	@Test
+	public void testMoveTile() {
+		board.setTile(new Position(2, 0), new Wall());
+		try {
+			board.moveTile(new Position(2, 0), new Position(2, 1));
+			assertEquals(EmptyTile.class, board.getTile(new Position(2, 0)).getClass());
+			assertEquals(Wall.class, board.getTile(new Position(2, 1)).getClass());
+		} catch (Exception e) {
+			fail();
 		}
 	}
 }
