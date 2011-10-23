448d0899c55ada89a5beaeb2a3c2c2db829847dc
Federico Bond <federicobond@gmail.com>
2011-10-22 22:23:38 -0300

    Añadidos primeros testeos de unidad

---
+++ src/tests/BoardTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import game.Board;
+import game.InvalidBoardSizeException;
+import junit.framework.TestCase;
+
+import misc.Position;
+
+import org.junit.Test;
+
+import tiles.EmptyTile;
+
+public class BoardTest extends TestCase {
+
+	private Board board;
+
+	public void setUp() {
+		board = new Board(10, 10);
+	}
+
+	@Test
+	public void testBlankBoard() {
+		assertTrue(board.getTile(new Position(0, 0)).getClass() == EmptyTile.class);
+	}
+
+	@Test
+	public void testInvalidBoardSize() {
+		try {
+			Board b = new Board(4, 4);
+			fail();
+		} catch (InvalidBoardSizeException e) {
+		}
+
+		try {
+			Board b = new Board(26, 26);
+			fail();
+		} catch (InvalidBoardSizeException e) {
+		}
+	}
+}
