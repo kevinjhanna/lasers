893345146ccf40545ac2f60d2e241a0de0ee4f2a
Federico Bond <federicobond@gmail.com>
2011-10-23 23:44:46 -0300

    Main simplificado

--- src/frontend/LasersAndMirrors.java
+++ src/frontend/LasersAndMirrors.java
@@ -1,17 +1,8 @@
 package frontend;
-import java.awt.EventQueue;
 
 public class LasersAndMirrors {
 	
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					new GameController(new Window());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		new GameController();
 	}
 }
