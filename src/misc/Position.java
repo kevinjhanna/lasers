3ec3998f626ec29b3d8675c02f457b8ede6bcf6d
Federico Bond <federicobond@gmail.com>
2011-10-29 14:10:26 -0300

    Added hashcode and equals to Position

--- src/misc/Position.java
+++ src/misc/Position.java
@@ -48,4 +48,29 @@ public class Position implements Serializable {
 		return "Position [row=" + row + ", column=" + column + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + column;
+		result = prime * result + row;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (column != other.column)
+			return false;
+		if (row != other.row)
+			return false;
+		return true;
+	}
+
 }
