01c1839972bb91c65a39ace6f4763b0c3d32dd91
Federico Bond <federicobond@gmail.com>
2011-10-30 13:45:13 -0300

    Renamed PositionOutOfBounds and added two methods to check for it.

--- src/exceptions/PositionOutOfBoundsException.java
+++ src/exceptions/PositionOutOfBoundsException.java
@@ -4,7 +4,7 @@ package exceptions;
  * Exception thrown when trying to access a position that lies outside of the
  * board boundaries.
  */
-public class PositionOutOfBounds extends RuntimeException {
+public class PositionOutOfBoundsException extends RuntimeException {
 
 	private static final long serialVersionUID = -6568636946390266381L;
 
